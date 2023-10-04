3a2a25d4a78ffe87f07d58094d1c607fdebc0353
raiane-oliveira <raiane.oliveira404@gmail.com>
2023-10-04 20:11:49 -0300

    add styles.ts on git history

---
+++ src/pages/schedule/[username]/ScheduleForm/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '@raiane-ignite-ui/react'
+
+export const ToastContainer = styled('div', {
+  position: 'absolute',
+  right: '$8',
+  bottom: '$8',
+  maxWidth: '22.5rem',
+  width: '100%',
+})
